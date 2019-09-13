c9cf559fbd76a7b70c5218d16a92d0026157dd97
Thierry Aronoff <thierry.aronoff@gmail.com>
2019-09-13 09:46:44 +0200

    Composant Liste Joueur

--- app/Components/JoueursComponents/Joueur.tsx
+++ app/Components/JoueursComponents/Joueur.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import {ScrollView, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
+import ListeJoueur from './ListeJoueurs';
 
 const Joueur = () => {
     return (
         <ScrollView style={styles.container}>
-            <TouchableOpacity style={styles.JoueurContainer}>
-                <Image source={{uri: "https://via.placeholder.com/100"}}
-                       style={styles.imageContainer}
-                />
-                <Text
-                    style={styles.textContainer}
-                > Nom du Joueur</Text>
-            </TouchableOpacity>
+            <ListeJoueur/>
         </ScrollView>
     )
 };
@@ -20,19 +14,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         display: "flex"
-    },
-    JoueurContainer: {
-        flex: 1
-    },
-    imageContainer: {
-        flex: 1,
-        height: 100,
-        width: 100
-    },
-    textContainer: {
-        flex: 2,
-        color: "#000"
     }
-})
+});
 
 export default Joueur;
